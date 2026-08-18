133cc75dcc81a3881a182b84613777f5f851f115
kinhdoanhmoc12-collab <kinhdoanhmoc12@gmail.com>
2026-08-18 16:37:25 +0700

    chore: migrate to astro ssr node

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,15 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://daoxuanquang.com.vn',
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
   integrations: [mdx(), sitemap()],
   devToolbar: {
     enabled: false,
@@ -15,3 +20,4 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
 });
+
